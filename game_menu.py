ebd5140b7adc2729685bb07cdbfe8880cceffa20
PrimakDmitriy <Primak.DN@phystech.edu>
2019-12-18 15:02:56 +0300

    ready_to_be_additional_modul

--- game_menu.py
+++ game_menu.py
@@ -3,21 +3,20 @@ import time
 from PIL import ImageTk, Image
 from pygame import mixer
 
-root = Tk()
-root.geometry('500x550')
-canv = Canvas(root,bg='white')
-canv.pack(fill=BOTH,expand=1)
-
 def create_main_menu():
-    global buttons
+    global buttons, win_menu, images
     if len(buttons) > 0:  #method bind doesn't work for music
         play_short_sound()
     canv.delete(all)  #deleting all, including buttons
     for b in buttons:
         b.destroy()
-        
+    for b in images:
+        canv.delete(b)
+
     img = ImageTk.PhotoImage(Image.open("Textures\menu.jpg"))   #the only way to create background in function
-    win = canv.create_image(0, 0, anchor=NW, image=img)
+    images.append(img)
+    win_menu = Label(text = '', image = img)
+    win_menu.place(x=0, y=0)
 
     play_intro_music()
 
@@ -25,16 +24,16 @@ def create_main_menu():
     button2 = Button(root, image=img2, command=create_settings_menu)   #start button
     button2.place(x=100, y=350)
     buttons.append(button2)
+    images.append(img2)
     
     img3 = ImageTk.PhotoImage(Image.open("Buttons/settings.jpg"))
     button3 = Button(root, image=img3, command=create_settings_menu)   #settings button
     button3.place(x=50, y=355)
     buttons.append(button3)
-    
-    win.pack()   #!doesn't work if being removed! Leave as the last string of the function!
+    images.append(img3)
     
 def create_settings_menu():
-    global buttons
+    global buttons, win_menu, images
     if len(buttons) > 0:  #method bind doesn't work for music
         play_short_sound()
     canv.delete(all)  #deleting all, including buttons
@@ -42,12 +41,15 @@ def create_settings_menu():
         b.destroy()
         
     img = ImageTk.PhotoImage(Image.open("Textures\settings.jpg"))
-    win = canv.create_image(0, 0, anchor=NW, image=img)
+    images.append(img)
+    win_menu = Label(text = '', image = img)
+    win_menu.place(x=0, y=0)
 
     img4 = ImageTk.PhotoImage(Image.open("buttons/back.jpg"))
     button4 = Button(root, image=img4, command=create_main_menu) #back to main menu button
     button4.place(x=20, y=505)
     buttons.append(button4)
+    images.append(img4)
 
     global sound_on_off
     sound_button = Checkbutton(text="Звук", command = change_sound_existance, variable = sound_on_off, onvalue=1, offvalue=0)
@@ -55,8 +57,6 @@ def create_settings_menu():
     buttons.append(sound_button)
     sound_button.deselect()
     sound_on_off = False
-    
-    win.pack()    #!doesn't work if being removed! Leave as the last string of the function!
 
 def play_short_sound():
     global sound_on_off
@@ -77,16 +77,20 @@ def change_sound_existance():
     sound_on_off = not(sound_on_off)
     print('&', sound_on_off)
 
-'''root = Tk()
-root.geometry('500x550')
+def open_game():
+    global root
+    root = Tk()
+    root.geometry('500x550')
+    global canv
+    canv = Canvas(root,bg='white')
+    canv.pack(fill=BOTH,expand=1)
+    global images
+    images = []
+    mixer.init()   #initialising mixer for playing music
+    global buttons
+    buttons =[]
+    global sound_on_off
+    sound_on_off = False
+    create_main_menu()
 
-canv = Canvas(root,bg='white')
-canv.pack(fill=BOTH,expand=1)
-
-img = ImageTk.PhotoImage(Image.open("Textures\menu.jpg"))
-canv.create_image(0, 0, anchor=NW, image=img)'''
-
-mixer.init()   #initialising mixer for playing music
-buttons =[]
-sound_on_off = False
-create_main_menu()
+open_game()
