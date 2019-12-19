1e9b376061b8538aa79815472fed11f718f2d0c1
PrimakDmitriy <Primak.DN@phystech.edu>
2019-12-19 03:31:35 +0300

    trying to fix bugs

--- Module_Arkitecture.py
+++ Module_Arkitecture.py
@@ -316,5 +316,38 @@ class Piece():
             return False
         return True
 
+class GameCanvas(Canvas):
+    def __init__(self, rt, wid, hei, bg):
+        self = Canvas(rt, width = wid, height = hei, background = bg)
+    
+    def drop_Piecees(self, Piecees_to_drop):
+        for Piece in Piecees_to_drop:
+            self.move(Piece, 0, Arkitecture.LENGTH)
+        self.update()    
+    
+    def clean_line(self, Deleting):
+        for Piece in Deleting:
+            self.delete(Piece)
+        self.update()
+
+    def completed_lines(self, YY):
+        cleaned_lines = 0
+        YY = sorted(YY)
+        for y in YY:
+            if sum(1 for Piece in self.find_withtag('game') if self.coords(Piece)[3] == y) == ((Arkitecture.width - 20) // Arkitecture.LENGTH): 
+                self.clean_line([Piece for Piece in self.find_withtag('game') if self.coords(Piece)[3] == y])
+                self.drop_Piecees([Piece for Piece in self.find_withtag('game') if self.coords(Piece)[3] < y])
+                cleaned_lines += 1
+        return cleaned_lines
+
+    def game_board(self):
+        board = [[0] * ((Arkitecture.width - 20) // Arkitecture.LENGTH) for _ in range(Arkitecture.height // Arkitecture.LENGTH)]
+        for Piece in self.find_withtag('game'):
+            x, y, _, _ = self.coords(Piece)
+            board[int(y // Arkitecture.LENGTH)][int(x // Arkitecture.LENGTH)] = 1
+        return board
+    def Piecees(self):
+        return self.find_withtag('game') == self.find_withtag(fill="blue")
+
 game = Arkitecture(True)
 game.start()
